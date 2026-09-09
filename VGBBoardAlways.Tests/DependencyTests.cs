c38213291e3ac219b4a9f496dc80399f670887db
Florian Kinder <florian.kinder@enthus.de>
2026-09-09 12:34:52 +0200

    test: pin API dependency identity and constructor shape

--- VGBBoardAlways.Tests/DependencyTests.cs
+++ VGBBoardAlways.Tests/DependencyTests.cs
@@ -16,6 +16,8 @@ public sealed class DependencyTests
         Assert.DoesNotContain(assembly.MainModule.AssemblyReferences, reference => reference.Name is "Assembly-CSharp" or "0Harmony" or "HarmonyX" or "VGModAPI.Core");
         var plugin = assembly.MainModule.GetType("VGBBoardAlways.Plugin");
         var dependency = Assert.Single(plugin.CustomAttributes.Where(attribute => attribute.AttributeType.FullName == "BepInEx.BepInDependency"));
+        Assert.Equal(2, dependency.ConstructorArguments.Count);
+        Assert.Equal(VGModAPI.ModApi.PluginId, dependency.ConstructorArguments[0].Value);
         Assert.Equal("0.2.0", dependency.ConstructorArguments[1].Value);
         Assert.Contains(assembly.MainModule.GetMemberReferences().OfType<MethodReference>(),
             reference => reference.DeclaringType.FullName == "VGModAPI.ModApi" && reference.Name == "get_Services");
